3761fadfa271bb6b65704579322f57b59154ec2f
ImaniJo-code <imani.l.johnson@gmail.com>
2021-06-12 21:46:13 -0400

    vars, prompts, and confirms added for characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,46 @@ function writePassword() {
 
 }
 
+// length prompt and confirm characters
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
+
+  var numbers = confirm("Do you want numbers in your password?");
+
+  var lowerCases = confirm("Do you want lowercases in your password?");
+
+  var upperCases = confirm("Do you want uppercases in your password?");
+
+  var special = confirm("Do you want special characters in your password?");
+
+  // min count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+
+
+  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
+
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
+
+
+  // Generator functions**
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+
+    getLowerCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+
+    getUpperCases: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+
+    getSpecialCharacters: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+
+};
